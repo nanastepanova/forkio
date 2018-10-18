afed78bb18916c6027c1e141a20959c062264a19
nanastepanova <chalenko.natalia@gmail.com>
2018-10-18 10:33:03 +0300

    add gulpfile.js and reset.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,42 @@
 'use strict'
 
 var gulp = require('gulp');
-var minify = require('gulp-uglify');
-var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+var runSequence = require('run-sequence');
 var clean = require('gulp-clean');
-var concat = require('gulp-concat');
-var imagemin = require('gulp-imagemin');
-var jsMinify = require("gulp-js-minify");
-var cleanCss = require("gulp-clean-css");
-var autoprefixer = require("gulp-autoprefixer");
+var gulpCopy = require('gulp-copy')
+
+gulp.task('clean', function(){
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+})
+
+gulp.task('serve', function (){
+    runSequence('clean', ['sass', 'copy'], function(){
+        browserSync.init({
+            server: "./"
+        })
+
+        gulp.watch('./src/scss/*.scss',['sass']).on('change', browserSync.reload);
+        gulp.watch('./index.html').on('change', browserSync.reload)
+    })
+})
+gulp.task('sass', function(){
+    return gulp.src('./src/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'))
+})
+
+gulp.task('copy', function(){
+    return gulp.src('./src/scss/*.css')
+        .pipe(gulp.dest('./dist/css/'))
+
+})
+
+gulp.task('default', ['serve'], function(){
+    console.log('=== ALL DONE ===')
+})
