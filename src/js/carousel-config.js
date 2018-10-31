b6ae5fa531f337deff14db437fa5913c986e10be
nanastepanova <chalenko.natalia@gmail.com>
2018-10-31 13:26:53 +0200

    changed html changed carousel-config.js changed _callback-section.scss changed _reviews-section.scss changed main.scss changed gulpfile.js add images in features

--- src/js/carousel-config.js
+++ src/js/carousel-config.js
@@ -1,26 +1,15 @@
 $(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
-        nav:true,
+    let owl = $('.owl-carousel');
+    owl.owlCarousel({
+        nav: false,
         loop: true,
         margin: 10,
         items: 1,
-        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
-        /*
-        responsiveClass: true,
-
-        responsive: {
-            0: {
-                items: 1,
-                nav: true
-            },
-            600: {
-                items: 2,
-                nav: true
-            },
-            1000: {
-                items: 3,
-                nav: true,
-            }
-        }*/
-    })
+    });
+    $("#carousel_prev").click(function () {
+        owl.trigger("prev.owl.carousel");
+    });
+    $("#carousel_next").click(function () {
+        owl.trigger("next.owl.carousel");
+    });
 });
